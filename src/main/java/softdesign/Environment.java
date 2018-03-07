2e282323902e3fb5f03b663729786acde0a8f5c0
Leroy <leroytruong@gmail.com>
2018-03-07 12:41:46 +0100

    Formatted and refactored Environment
    - Autoformatted
    - Reduced access modifiers
    - Removed `this` in constructor for methods
    - Renamed `#initializeWalls` to `#initWalls` to stay consistent with other similar init methods
    - Moved comment to JavaDocs paragraph in `#initWalls`
    - Renamed `#initializeObstacles` to `#initObstacles` to stay consistent with other similar `init...` methods
    - Removed redundant method `#coords(int, int)`

--- src/main/java/softdesign/Environment.java
+++ src/main/java/softdesign/Environment.java
@@ -12,61 +12,59 @@ import simbad.sim.EnvironmentDescription;
 import simbad.sim.Wall;
 
 public class Environment extends EnvironmentDescription {
-	
-	public static final int WORLD_SIZE = 10;
-  public static final int SMALL = 10;
-  public static final int LARGE = 30;
-	
-	public Environment() {
-    this.light1IsOn = true;
-    this.light2IsOn = true;
-    this.showAxis(true);
-    this.setWorldSize(WORLD_SIZE);
-    this.initializeWalls();
-  }
 
-  public Vector3d coords(int x, int y) {
-      return new Vector3d(-y, 0, -x);
-  } 
-    
-  void initializeWalls() {
+    public static final int WORLD_SIZE = 10;
+    public static final int SMALL = 10;
+    public static final int LARGE = 30;
 
-	// The walls are now placed at the limit of the world size and the walls
-	// will change in response to the size of the World
+    Environment() {
+        this.light1IsOn = true;
+        this.light2IsOn = true;
 
-    Wall w1 = new Wall(new Vector3d(-WORLD_SIZE /2, 0, 0), WORLD_SIZE, 2, this);
-    w1.setColor(new Color3f(Color.BLUE));
-    w1.rotate90(1);
-    add(w1);
-    
-    Wall w2 = new Wall(new Vector3d(WORLD_SIZE / 2, 0, 0), WORLD_SIZE, 2, this);
-    w2.setColor(new Color3f(Color.GREEN));
-    w2.rotate90(1);
-    add(w2);
-    
-    Wall w3 = new Wall(new Vector3d(0, 0, WORLD_SIZE / 2), WORLD_SIZE, 2, this);
-    w3.setColor(new Color3f(Color.RED));
-    add(w3);
-    
-    Wall w4 = new Wall(new Vector3d(0, 0, -WORLD_SIZE / 2), WORLD_SIZE, 2, this);
-    w4.setColor(new Color3f(Color.YELLOW));
-    add(w4);
-  }
+        showAxis(true);
+        setWorldSize(WORLD_SIZE);
+        initWalls();
+    }
 
-  public void initializeObstacles() {
-    Box box1 = new Box(randomVector(), new Vector3f(1, 1, 1), this);
-    box1.setColor(new Color3f(Color.ORANGE));
-    add(box1);
-    
-    Box box2 = new Box(randomVector(), new Vector3f(1, 1, 1), this);
-    box2.setColor(new Color3f(Color.ORANGE));
-    add(box2);
-  }
+    /**
+     *  Initializes the walls.
+     *
+     *  The walls are now placed at the limit of the world size and the walls will change in response to the size of
+     *  the world
+     */
+    private void initWalls() {
+        Wall w1 = new Wall(new Vector3d(-WORLD_SIZE / 2, 0, 0), WORLD_SIZE, 2, this);
+        w1.setColor(new Color3f(Color.BLUE));
+        w1.rotate90(1);
+        add(w1);
 
-  public Vector3d randomVector() {
-    int randomX = ThreadLocalRandom.current().nextInt(-WORLD_SIZE/2, WORLD_SIZE/2 + 1);
-    int randomY = ThreadLocalRandom.current().nextInt(-WORLD_SIZE/2, WORLD_SIZE/2 + 1);
-    return coords(randomX, randomY);
-  }
+        Wall w2 = new Wall(new Vector3d(WORLD_SIZE / 2, 0, 0), WORLD_SIZE, 2, this);
+        w2.setColor(new Color3f(Color.GREEN));
+        w2.rotate90(1);
+        add(w2);
 
+        Wall w3 = new Wall(new Vector3d(0, 0, WORLD_SIZE / 2), WORLD_SIZE, 2, this);
+        w3.setColor(new Color3f(Color.RED));
+        add(w3);
+
+        Wall w4 = new Wall(new Vector3d(0, 0, -WORLD_SIZE / 2), WORLD_SIZE, 2, this);
+        w4.setColor(new Color3f(Color.YELLOW));
+        add(w4);
+    }
+
+    public void initObstacles() {
+        Box box1 = new Box(randomVector(), new Vector3f(1, 1, 1), this);
+        box1.setColor(new Color3f(Color.ORANGE));
+        add(box1);
+
+        Box box2 = new Box(randomVector(), new Vector3f(1, 1, 1), this);
+        box2.setColor(new Color3f(Color.ORANGE));
+        add(box2);
+    }
+
+    private Vector3d randomVector() {
+        int randomX = ThreadLocalRandom.current().nextInt(-WORLD_SIZE / 2, WORLD_SIZE / 2 + 1);
+        int randomY = ThreadLocalRandom.current().nextInt(-WORLD_SIZE / 2, WORLD_SIZE / 2 + 1);
+        return new Vector3d(randomY, 0, randomX);
+    }
 }
