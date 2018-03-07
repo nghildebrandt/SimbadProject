b028ad68b24f9ba927931b1ae09827b81cae5ec7
Leroy <leroytruong@gmail.com>
2018-03-07 12:15:47 +0100

    Formatted and refactored CentralBase
    - Autoformatted
    - Removed redundant static global variable `world_size`
    - Moved static global constant `WALL` to class `Map`

--- src/main/java/softdesign/CentralBase.java
+++ src/main/java/softdesign/CentralBase.java
@@ -1,47 +1,46 @@
 package main.java.softdesign;
 
 public class CentralBase {
-  Map map = new Map();
-  private static int world_size = Environment.WORLD_SIZE;
-  public static final int WALL = -1;
+    
+    private Map map = new Map();
 
-  public CentralBase() {
-		try {
-			mapWalls();
-      mapImaginaryWalls();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-  }
-
-	public void mapWalls() throws Exception {
-		for (int i = 0; i <= world_size/2; i++) {
-			map.setPoint(i, world_size/2, WALL);
-			map.setPoint(-i, world_size/2, WALL);
-			map.setPoint(i, -world_size/2, WALL);
-			map.setPoint(-i, -world_size/2, WALL);
-			map.setPoint(world_size/2, i, WALL);
-			map.setPoint(world_size/2, -i, WALL);
-			map.setPoint(-world_size/2, i, WALL);
-			map.setPoint(-world_size/2, -i, WALL);
-		}
-	}
-
-  public void mapImaginaryWalls() throws Exception {
-    for (int i = 0; i <= world_size/2; i ++) {
-      map.setPoint(0, i, -1);
-      map.setPoint(0, -i, -1);
-      map.setPoint(i, 0, -1);
-      map.setPoint(-i, 0, -1 );
+    CentralBase() {
+        try {
+            mapWalls();
+            mapImaginaryWalls();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-  }
 
-  public boolean isMissionComplete() {
-    return true;
-  }
+    private void mapWalls() throws Exception {
+        for (int i = 0; i <= Environment.WORLD_SIZE / 2; i++) {
+            map.setPoint(i, Environment.WORLD_SIZE / 2, Map.WALL);
+            map.setPoint(-i, Environment.WORLD_SIZE / 2, Map.WALL);
+            map.setPoint(i, -Environment.WORLD_SIZE / 2, Map.WALL);
+            map.setPoint(-i, -Environment.WORLD_SIZE / 2, Map.WALL);
+            map.setPoint(Environment.WORLD_SIZE / 2, i, Map.WALL);
+            map.setPoint(Environment.WORLD_SIZE / 2, -i, Map.WALL);
+            map.setPoint(-Environment.WORLD_SIZE / 2, i, Map.WALL);
+            map.setPoint(-Environment.WORLD_SIZE / 2, -i, Map.WALL);
+        }
+    }
 
-  //used to give copy to robots
-  public Map sendMap() {
-    return map;
-  }
+    private void mapImaginaryWalls() throws Exception {
+        for (int i = 0; i <= Environment.WORLD_SIZE / 2; i++) {
+            map.setPoint(0, i, -1);
+            map.setPoint(0, -i, -1);
+            map.setPoint(i, 0, -1);
+            map.setPoint(-i, 0, -1);
+        }
+    }
+
+    public boolean isMissionComplete() {
+        return true;
+    }
+
+    //used to give copy to robots
+    public Map sendMap() {
+        return map;
+    }
 }
