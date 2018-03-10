305ec8718345720e214d8270be71b5da16bc2dca
leroy <leroytruong@gmail.com>
2018-03-10 20:34:19 +0100

    Compatibility adjustments

--- src/main/java/softdesign/Main.java
+++ src/main/java/softdesign/Main.java
@@ -11,10 +11,10 @@ public class Main {
 
 	private void start() {
 		CentralBase centralBase = new CentralBase();
-		Map map = centralBase.sendMap();
+		Map map = centralBase.copyMap();
 		List<Agent> swarm = new ArrayList<>();
 
-		if (Environment.WORLD_SIZE <= Environment.LARGE) {
+		if (Environment.SIZE <= Environment.LARGE) {
 			swarm.add(new Robot(new Vector3d(8, 0, 8), "small", map, Robot.SOUTH));
 			swarm.add(new Robot(new Vector3d(8, 0, -8), "small", map, Robot.SOUTH));
 			swarm.add(new Robot(new Vector3d(-8, 0, -8), "small", map, Robot.SOUTH));
