9296dc934eb360f7734395987f69b3ad9d4dd17a
Leroy <leroytruong@gmail.com>
2018-03-14 14:19:02 +0100

    Added JavaDocs

--- src/main/java/softdesign/image/ImageRepositoryFactory.java
+++ src/main/java/softdesign/image/ImageRepositoryFactory.java
@@ -1,10 +1,18 @@
 package main.java.softdesign.image;
 
+/**
+ * Factory for creating instances of {@link ImageRepository}.
+ */
 public final class ImageRepositoryFactory {
 
 	private ImageRepositoryFactory() {
 	}
 
+	/**
+	 * Returns the default in-memory implementation.
+	 *
+	 * @return default implementation
+	 */
 	public static ImageRepository get() {
 		return new ImageArrayListStore();
 	}
