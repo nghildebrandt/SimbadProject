78766efb944314e2a69aaf87110f4b8243180033
leroy <leroytruong@gmail.com>
2018-03-30 23:14:24 +0200

    Cleanup Main
    - Removed unused counter
    - Extracted variable

--- src/main/java/softdesign/Main.java
+++ src/main/java/softdesign/Main.java
@@ -4,18 +4,14 @@ public class Main {
 
 	private static final long REPORT_INTERVAL = 500L;
 
-	int stepCounter = 0;
-
 	private void start() throws InterruptedException {
 		Environment environment = Environment.getInstance();
 		CentralStation centralStation = CentralStation.getInstance();
 		centralStation.startMission(environment);
 
 		while (!centralStation.isMissionComplete()) {
-			stepCounter++;
-
-			System.out.println(centralStation.getMissionProgress());
-
+			double missionCompletionPercentage = centralStation.getMissionProgress();
+			System.out.println(missionCompletionPercentage);
 			Thread.sleep(REPORT_INTERVAL);
 		}
 	}
